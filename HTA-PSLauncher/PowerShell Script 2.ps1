06d698b17658b5bfeff405620386bf8990639f3e
LesFerch <79026235+LesFerch@users.noreply.github.com>
2022-02-18 00:24:51 -0500

    Add files via upload

---
+++ HTA-PSLauncher/PowerShell Script 2.ps1
@@ -0,0 +1,2 @@
+  Write-Host `n'This is script #2'`n
+  Read-Host -Prompt 'Press any key to continue'
---
+++ HTA-PSLauncher/PowerShell Script 3.ps1
@@ -0,0 +1,2 @@
+  Write-Host `n'This is script #3'`n
+  Read-Host -Prompt 'Press any key to continue'
