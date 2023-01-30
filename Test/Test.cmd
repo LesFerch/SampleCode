7f9619862fc81596f6d0e479788f1094b4ed1106
LesFerch <79026235+LesFerch@users.noreply.github.com>
2023-01-29 20:45:24 -0500

    Add files via upload

--- Test/Test.cmd
+++ Test/Test.cmd
@@ -1,5 +1,5 @@
 @Echo Off
 CD /D %~dp0
-MD AppData 2>Nul
-Move Test.txt AppData\Test.ini 2>Nul
+MD AppData >Nul 2>Nul
+Move Test.txt AppData\Test.ini >Nul 2>Nul
 PowerShell -ExecutionPolicy Bypass .\Test.ps1 AppData\Test.ini
